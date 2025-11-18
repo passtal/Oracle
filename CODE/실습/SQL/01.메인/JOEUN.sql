62220f27197662be330555f979b334d64bc621b0
passtal <orbiyoung@gmail.com>
2025-11-18 18:19:20 +0900

    251118

--- CODE/실습/SQL/01.메인/JOEUN.sql
+++ CODE/실습/SQL/01.메인/JOEUN.sql
@@ -204,3 +204,369 @@ WHERE e.DEPT_CODE = d.DEPT_ID
                             WHERE DEPT_CODE = 'D9'
                         )
 ;
+
+-- 88.
+-- EMPLOYEE 와 DEPARTMENT 테이블을 조인하여 출력하되,
+-- 부서가 없는 직원도 포함하여 출력하시오.
+-- 사원번호, 직원명, 부서번호, 부서명
+SELECT e.emp_id 사원번호
+      ,e.emp_name 직원명
+      ,NVL(d.dept_id, '(없음)') 부서번호
+      ,NVL(d.dept_title, '(없음)') 부서명
+FROM employee e
+    LEFT JOIN department d
+    ON (e.dept_code = d.dept_id)
+;
+
+-- 89.
+-- EMPLOYEE 와 DEPARTMENT 테이블을 조인하여 출력하되,
+-- 직원이 없는 부서도 포함하여 출력하시오.
+SELECT NVL(e.emp_id, '(없음)') 사원번호
+      ,NVL(e.emp_name, '(없음)') 직원명
+      ,d.dept_id 부서번호
+      ,d.dept_title 부서명
+FROM employee e
+    RIGHT JOIN department d
+    ON (e.dept_code = d.dept_id)
+;
+
+--90.
+-- 직원 및 부서 유무에 상관없이 출력하는 SQL문을 작성하시오.
+SELECT NVL(e.emp_id, '(없음)') 사원번호
+      ,NVL(e.emp_name, '(없음)') 직원명
+      ,NVL(d.dept_id, '(없음)') 부서번호
+      ,NVL(d.dept_title, '(없음)') 부서명
+FROM employee e
+    FULL JOIN department d
+    ON (e.dept_code = d.dept_id)
+;
+
+-- 91.
+-- 사원번호, 직원명, 부서번호, 지역명, 국가명, 급여, 입사일자를 출력하시오.
+SELECT * FROM EMPLOYEE;
+SELECT * FROM DEPARTMENT;
+SELECT * FROM LOCATION;
+SELECT * FROM NATIONAL;
+
+SELECT
+    E.EMP_ID 사원번호,
+    E.EMP_NAME 직원명,
+    D.DEPT_ID 부서번호,
+    D.DEPT_TITLE 부서명,
+    L.LOCAL_CODE 지역명,
+    N.NATIONAL_NAME 국가명,
+    E.SALARY 급여
+FROM EMPLOYEE E
+    LEFT JOIN DEPARTMENT D ON E.DEPT_CODE = D.DEPT_ID
+    LEFT JOIN LOCATION L ON D.LOCATION_ID = L.LOCAL_CODE
+    LEFT JOIN NATIONAL N ON L.NATIONAL_CODE = N.NATIONAL_CODE
+;
+
+-- 92.
+-- 사원들 중 매니저를 출력하시오.
+-- 사원번호, 직원명, 부서명, 직급, 구분('매니저')
+-- * MANAGER_ID : 해당 사원의 매니저 사원번호
+SELECT * FROM employee;
+
+-- 1.
+-- MANAGER_ID 컬럼이 NULL 이 아닌 사원을 중복없이 조회
+-- -> 매니저들의 사원 번호
+SELECT DISTINCT MANAGER_ID
+FROM EMPLOYEE
+WHERE MANAGER_ID IS NOT NULL
+;
+
+-- 2.
+-- EMPLOYEE, DEPARTMENT, JOB 테이블을 조인하여 조회
+SELECT *
+FROM EMPLOYEE E
+    LEFT JOIN DEPARTMENT D ON E.DEPT_CODE = D.DEPT_ID
+    JOIN JOB J ON E.JOB_CODE = J.JOB_CODE
+;
+
+-- 3.
+-- 조인 결과 중, EMP_ID 가 매니저 사원번호인 경우만 조회
+SELECT 
+    E.EMP_ID 사원번호,
+    E.EMP_NAME 직원명,
+    D.DEPT_TITLE 부서명,
+    JOB_NAME 직급명,
+    '매니저' 구분
+FROM EMPLOYEE E
+    LEFT JOIN DEPARTMENT D ON E.DEPT_CODE = D.DEPT_ID
+    JOIN JOB J ON E.JOB_CODE = J.JOB_CODE
+WHERE E.EMP_ID IN (
+    SELECT DISTINCT MANAGER_ID
+    FROM EMPLOYEE
+    WHERE MANAGER_ID IS NOT NULL
+)
+;
+
+-- 93.
+-- 사원(매니저가 아닌)만 조회하시오.
+SELECT 
+    E.EMP_ID 사원번호,
+    E.EMP_NAME 직원명,
+    D.DEPT_TITLE 부서명,
+    JOB_NAME 직급명,
+    '사원' 구분
+FROM EMPLOYEE E
+    LEFT JOIN DEPARTMENT D ON E.DEPT_CODE = D.DEPT_ID
+    JOIN JOB J ON E.JOB_CODE = J.JOB_CODE
+WHERE E.EMP_ID NOT IN (
+    SELECT DISTINCT MANAGER_ID
+    FROM EMPLOYEE
+    WHERE MANAGER_ID IS NOT NULL
+)
+;
+
+-- 94.
+-- UNION 키워드를 사용하여,
+-- 매니저와 사원 구분하여 조회하시오.
+SELECT 
+    E.EMP_ID 사원번호,
+    E.EMP_NAME 직원명,
+    D.DEPT_TITLE 부서명,
+    JOB_NAME 직급명,
+    '매니저' 구분
+FROM EMPLOYEE E
+    LEFT JOIN DEPARTMENT D ON E.DEPT_CODE = D.DEPT_ID
+    JOIN JOB J ON E.JOB_CODE = J.JOB_CODE
+WHERE E.EMP_ID IN (
+    SELECT DISTINCT MANAGER_ID
+    FROM EMPLOYEE
+    WHERE MANAGER_ID IS NOT NULL
+)
+UNION
+SELECT 
+    E.EMP_ID 사원번호,
+    E.EMP_NAME 직원명,
+    D.DEPT_TITLE 부서명,
+    JOB_NAME 직급명,
+    '사원' 구분
+FROM EMPLOYEE E
+    LEFT JOIN DEPARTMENT D ON E.DEPT_CODE = D.DEPT_ID
+    JOIN JOB J ON E.JOB_CODE = J.JOB_CODE
+WHERE E.EMP_ID NOT IN (
+    SELECT DISTINCT MANAGER_ID
+    FROM EMPLOYEE
+    WHERE MANAGER_ID IS NOT NULL
+)
+;
+
+-- 95.
+-- CASE 키워드를 사용하여,
+-- 매니저와 사원 구분하여 조회하시오.
+SELECT 
+    E.EMP_ID 사원번호,
+    E.EMP_NAME 직원명,
+    D.DEPT_TITLE 부서명,
+    JOB_NAME 직급명,
+    CASE
+        WHEN E.EMP_ID IN (
+            SELECT DISTINCT MANAGER_ID
+            FROM EMPLOYEE
+            WHERE MANAGER_ID IS NOT NULL
+            )
+        THEN '매니저'
+        ELSE '사원'
+    END 구분
+FROM EMPLOYEE E
+    LEFT JOIN DEPARTMENT D ON E.DEPT_CODE = D.DEPT_ID
+    JOIN JOB J ON E.JOB_CODE = J.JOB_CODE
+;
+
+-- 96 & 97
+-- EMP_NO : 주민등록번호
+-- HIRE_DATE : 입사연도
+-- 주민등록번호 뒷자리가 1또는3으로 시작 - 남성
+-- 주민등록번호 뒷자리가 2또는4로 시작 - 여성
+
+SELECT * FROM EMPLOYEE;     -- 변수명 조회
+
+SELECT 
+    E.EMP_ID 사원번호,
+    E.EMP_NAME 직원명,
+    D.DEPT_TITLE 부서명,
+    J.JOB_NAME 직급명,
+
+    -- 매니저 사원 구분
+    CASE
+        WHEN E.EMP_ID IN (
+            SELECT DISTINCT MANAGER_ID
+            FROM EMPLOYEE
+            WHERE MANAGER_ID IS NOT NULL
+            )
+        THEN '매니저'
+        ELSE '사원'
+    END 구분,
+
+    -- 성별
+    -- * 주민등록번호 (EMP_NO) 뒷자리 첫 글자
+    -- 1,3 (남자), 2,4(여자)
+    CASE
+        WHEN 
+            SUBSTR(EMP_NO, 8, 1) IN ('1', '3') THEN '남자'
+        ELSE '여자'
+    END 성별,
+
+    -- 현재나이
+    TRUNC(MONTHS_BETWEEN(sysdate, TO_DATE(
+        CASE
+            WHEN SUBSTR (EMP_NO, 8, 1) IN ('1', '2') THEN '19' || SUBSTR (EMP_NO, 1, 2)
+            WHEN SUBSTR (EMP_NO, 8, 1) IN ('3', '4') THEN '20' || SUBSTR (EMP_NO, 1, 2)
+    END, 'YYYY')) / 12) + 1 현재나이,
+
+    -- 만나이
+    TRUNC(MONTHS_BETWEEN(sysdate, TO_DATE(
+        CASE
+            WHEN SUBSTR (EMP_NO, 8, 1) IN ('1', '2') THEN '19' || SUBSTR (EMP_NO, 1, 6)
+            WHEN SUBSTR (EMP_NO, 8, 1) IN ('3', '4') THEN '20' || SUBSTR (EMP_NO, 1, 6)
+    END, 'YYYYMMDD')) / 12) 만나이,
+
+    --근속연수
+    TRUNC(MONTHS_BETWEEN(SYSDATE, HIRE_DATE) / 12) 근속연수,
+
+    -- 주민번호 마스킹
+    RPAD(SUBSTR(E.EMP_NO, 1, 8), 14, '*') 주민등록번호,
+
+    -- 입사일자
+    TO_CHAR(HIRE_DATE, 'YYYY.MM.DD') 입사일자,
+
+    -- 연봉
+    TO_CHAR((SALARY + (SALARY + NVL(BONUS, 0))) * 12, '999,999,999,999') 연봉
+
+FROM EMPLOYEE E
+    LEFT JOIN DEPARTMENT D ON E.DEPT_CODE = D.DEPT_ID
+    JOIN JOB J USING (JOB_CODE)
+;
+
+-- 98.
+-- employee, department 테이블을 조인하여,
+-- 사원번호, 직원명, 부서번호, 부서명, 이메일, 전화번호
+-- 주민번호, 입사일자, 급여, 연봉을 조회하시오.
+-- CREATE OR REPLACE 객체
+-- - 없으면, 새로 생성
+-- - 있으면, 대체 (기존에 생성 되어 있어도 에러발생X)
+CREATE OR REPLACE VIEW VE_EMP_DEPT AS
+SELECT e.emp_id
+      ,e.emp_name
+      ,d.dept_id
+      ,d.dept_title
+      ,e.email
+      ,e.phone
+
+      -- 주민등록번호
+      ,RPAD( SUBSTR(emp_no, 1, 8), 14, '*' ) emp_no
+
+      -- 입사일자
+      ,TO_CHAR( hire_date, 'YYYY.MM.DD' ) hire_date
+
+      -- 급여
+      ,TO_CHAR( salary, '999,999,999' ) salary
+
+      -- 연봉
+      ,TO_CHAR( (salary + NVL( salary*bonus, 0)) * 12, '999,999,999,999') yr_salary
+FROM employee e
+     LEFT JOIN department d ON (e.dept_code = d.dept_id)
+;
+
+-- 뷰 조회
+SELECT *
+FROM VE_EMP_DEPT
+;
+
+-- 99.
+-- 시퀀스를 생성하시오.
+-- SEQ_MS_USER
+-- SEQ_MS_BOARD
+-- SEQ_MS_FILE
+-- SEQ_MS_REPLY
+-- (시작: 1, 증가값: 1, 최솟값: 1, 최댓값: 1000000)
+-- 시퀀스 생성
+CREATE SEQUENCE SEQ_MS_USER
+INCREMENT BY 1                  -- 증가값
+START WITH 1                    -- 시작값
+MINVALUE 1                      -- 최솟값
+MAXVALUE 1000000                -- 최댓값
+;
+
+CREATE SEQUENCE SEQ_MS_BOARD
+INCREMENT BY 1                  -- 증가값
+START WITH 1                    -- 시작값
+MINVALUE 1                      -- 최솟값
+MAXVALUE 1000000                -- 최댓값
+;
+
+CREATE SEQUENCE SEQ_MS_FILE
+INCREMENT BY 1                  -- 증가값
+START WITH 1                    -- 시작값
+MINVALUE 1                      -- 최솟값
+MAXVALUE 1000000                -- 최댓값
+;
+
+CREATE SEQUENCE SEQ_MS_REPLY
+INCREMENT BY 1                  -- 증가값
+START WITH 1                    -- 시작값
+MINVALUE 1                      -- 최솟값
+MAXVALUE 1000000                -- 최댓값
+;
+
+-- 100.
+-- SEQ_MS_USER 의 다음 번화와 현재 번호를 출력하시오.
+
+-- 다음 시퀀스 번호
+SELECT SEQ_MS_USER.NEXTVAL
+FROM DUAL
+;
+
+-- 현재 시퀀스 번호
+SELECT SEQ_MS_USER.CURRVAL
+FROM DUAL
+;
+
+-- 101.
+-- SEQ_MS_USER 를 삭제하시오.
+DROP SEQUENCE SEQ_MS_USER;
+
+-- 102.
+-- SEQ_MS_USER 를 이용하여, MS_USER 의 user_no 가
+-- 시퀀스 번호로 적용될 수 있도록 데이터를 추가해보시오.
+INSERT INTO MS_USER (USER_NO, USER_ID, USER_PW, USER_NAME,
+        BIRTH, TEL, ADDRESS, REG_DATE,UPD_DATE, CTZ_NO, GENDER
+        )
+VALUES (
+        SEQ_MS_USER.NEXTVAL, 'ALOHA', '123456', '김조은',
+      '2024/03/06', '010-1234-1234', '인천 부평구', SYSDATE, SYSDATE,
+      '020101-4123123', '여'
+);
+
+INSERT INTO MS_USER (USER_NO, USER_ID, USER_PW, USER_NAME,
+        BIRTH, TEL, ADDRESS, REG_DATE,UPD_DATE, CTZ_NO, GENDER
+        )
+VALUES (
+        SEQ_MS_USER.NEXTVAL, 'JOEUN', '123456', '박조은',
+      '2024/03/06', '010-1234-1234', '서울 구로구', SYSDATE, SYSDATE,
+      '970101-2123123', '여'
+);
+
+-- 103.
+-- 시퀀스 SEQ_MS_USER 의 최댓값을 100,000,000 으로 수정하시오.
+ALTER SEQUENCE SEQ_MS_USER MAXVALUE 100000000;
+
+-- 104.
+-- USER_IND_COLUMNS 테이블을 조회하시오.
+-- * 사용자가 정의한 인덱스 정보가 들어있다.
+SELECT INDEX_NAME, TABLE_NAME, COLUMN_NAME
+FROM USER_IND_COLUMNS
+;
+
+-- 105.
+-- MS_USER 테이블의 USER_NAME 에 대한
+-- 인덱스 IDX_MS_USER_NAME 을 생성하시오.
+
+-- 인덱스 생성
+CREATE INDEX IDX_MS_USER_NAME ON MS_USER (USER_NAME);
+
+-- 인덱스 삭제
+DROP INDEX IDX_MS_USER_NAME;
